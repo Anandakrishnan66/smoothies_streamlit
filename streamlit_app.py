df650da6eaafeb67714a260a37f6ba39dbbabb94
AnandaKrishnan <85782904+Anandakrishnan66@users.noreply.github.com>
2025-04-15 15:00:43 +0530

    Update streamlit_app.py to create dataframe from requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
+sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True) 
+
 # Write directly to the app
 st.title(f"Customize your smoothie :cup_with_straw: ")
 st.write(
